2a612f0093a48be4fcc9b017287f8c5ad3a8a369
freegastello <freegastello@gmail.com>
2021-11-06 19:29:00 +0300

    211106 - Parcer Excel .xls и .xlsx

---
+++ src/main/java/com/glushkoproduction/note/testLessons/exampleTwo/MainClassParcerExcel.java
@@ -0,0 +1,11 @@
+package com.glushkoproduction.note.testLessons.exampleTwo;
+
+public class MainClassParcerExcel {
+    public static void main(String[] args){
+//        System.out.println(ExcelParser.parse("d:\\temp\\MyFirstExcel.xls").toString());
+//        System.out.println(ExcelParserNew.parse("d:\\temp\\MyFirstExcel.xls").toString());
+//        System.out.println(ExcelParser_2.parse("d:\\temp\\MyFirstExcel.xls").toString());
+        System.out.println(ExcelParser_3.parse("d:\\temp\\MyFirstExcel.xls").toString());
+//        System.out.println(ExcelParser_3.parse("d:\\temp\\MyFirstExcel_3.xlsx").toString());
+    }
+}
