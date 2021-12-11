ea1105ffa7b9ae9fb0738fcfaebda0bbe1b9a7d8
freegastello <freegastello@gmail.com>
2021-12-11 17:03:30 +0300

    211211 - selenium

--- src/test/java/com/glushkoproduction/note/selenium/Selenium.java
+++ src/test/java/com/glushkoproduction/note/selenium/Selenium.java
@@ -1,10 +1,51 @@
 package com.glushkoproduction.note.selenium;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class Selenium {
+
+    @Test
+    public void selenium_1_Test() {
+        System.setProperty("webdriver.chrome.driver","D:\\YandexDisk\\IdaProject\\Chromedriver\\chromedriver.exe");
+        String baseUrl = "http://localhost:9191/all";
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get(baseUrl);
+
+        WebElement inp1 = driver.findElement(By.cssSelector("input[name='num']"));
+        inp1.sendKeys("mmm");
+
+        WebElement inp2 = driver.findElement(By.cssSelector("input[name='year']"));
+        inp2.sendKeys("25");
+
+        WebElement inp3 = driver.findElement(By.cssSelector("input[name='name']"));
+        inp3.sendKeys("nnn");
+
+        WebElement btn1 = driver.findElement(By.xpath("//button[text()='Сохранить']"));
+        btn1.click();
+    }
+
+
+    @Test
+    public void seleniumPageTest() {
+        System.setProperty("webdriver.chrome.driver","D:\\YandexDisk\\IdaProject\\Chromedriver\\chromedriver.exe");
+        String baseUrl = "http://localhost:9191/all";
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        driver.get(baseUrl);
+
+
+    }
+
+
 
     @Test
     public void seleniumTest() {
